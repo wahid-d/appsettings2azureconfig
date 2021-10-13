de1d00c7b2498e2b6236e0df4549ef4dd16d25bd
David Wahid <wakhid2802@gmail.com>
2021-10-13 21:22:06 +0500

    Updated Github Url

--- Components/GithubPrompt.razor.cs
+++ Components/GithubPrompt.razor.cs
@@ -2,6 +2,6 @@ namespace settings2config.Components
 {
     public partial class GithubPrompt
     {
-        public string GithubUrl { get; set; } = "https://github.com/wahid-d/settings2config";
+        public string GithubUrl { get; set; } = "https://github.com/wahid-d/appsettings2azureconfig";
     }
 }
